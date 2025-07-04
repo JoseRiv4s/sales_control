68474a47b399f7ea149c5b392bc855496ea37019
JoseRiv4s <navedasjose@gmail.com>
2025-07-04 16:43:36 -0500

    chore: initial commit with Spring Boot project structure

---
+++ src/main/java/com/sales_control/rest/sales_api/SalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.sales_control.rest.sales_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sales_control/rest/sales_api/SalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sales_control.rest.sales_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
