153f986292f745a7ae70be3a39123f6965bb0d1e
JoseRiv4s <navedasjose@gmail.com>
2025-08-13 11:18:29 -0500

    feat: implement home controller with JWT authentication

--- src/main/java/com/sales_control/rest/sales_api/controller/HomeController.java
+++ src/main/java/com/sales_control/rest/sales_api/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.sales_control.rest.sales_api.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/sales")
-    public String welcome(Authentication authentication) {
-        return "Welcome " + authentication.getName() + " from secure endpoint";
+    public void home() {
     }
 }
