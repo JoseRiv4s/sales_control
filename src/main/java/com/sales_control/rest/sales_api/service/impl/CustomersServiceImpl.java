d8ace8d67c7519db5b010cc34377c94c75e48e60
JoseRiv4s <navedasjose@gmail.com>
2025-10-13 20:02:09 -0500

    fix: corregida asignación de ID en updateCustomer en CustomersServiceImpl

--- src/main/java/com/sales_control/rest/sales_api/service/impl/CustomersServiceImpl.java
+++ src/main/java/com/sales_control/rest/sales_api/service/impl/CustomersServiceImpl.java
@@ -86,6 +86,7 @@ public class CustomersServiceImpl implements CustomersService {
         CustomerEntity updatedCustomer = customersRepository.save(customer);
 
         CustomerResponseDTO updateResponse = new CustomerResponseDTO();
+        updateResponse.setCustomerId(updatedCustomer.getCustomerId());
         updateResponse.setFirstName(updatedCustomer.getFirstName());
         updateResponse.setLastName(updatedCustomer.getLastName());
         updateResponse.setAddress(updatedCustomer.getAddress());
@@ -125,11 +126,13 @@ public class CustomersServiceImpl implements CustomersService {
                 .stream()
                 .map(customer -> {
                     CustomerResponseDTO customerResponse = new CustomerResponseDTO();
+                    customerResponse.setCustomerId(customer.getCustomerId());
                     customerResponse.setFirstName(customer.getFirstName());
                     customerResponse.setLastName(customer.getLastName());
                     customerResponse.setAddress(customer.getAddress());
                     customerResponse.setEmail(customer.getEmail());
                     customerResponse.setPhone(customer.getPhone());
+                    customerResponse.setDocumentId(customer.getDocumentId());
                     log.info("Termina metodo findAllCustomers en CustomersServiceImpl");
                     return customerResponse;
 
